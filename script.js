dc507f07d3f86f65267c4aebf9030644b40a3669
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-04 14:21:48 +0200

    Create Envelope message

---
+++ script.js
@@ -0,0 +1,5 @@
+function openEnvelope() {
+    document.querySelector('.flap').style.transform = 'rotateX(-180deg)';
+    document.querySelector('.heart').style.opacity = '0';
+    document.querySelector('.message').style.opacity = '1';
+}
